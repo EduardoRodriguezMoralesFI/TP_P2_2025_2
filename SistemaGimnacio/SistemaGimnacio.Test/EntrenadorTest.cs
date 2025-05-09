4816bf38ccf364da5f65855a6f7131f464aa2904
EduardoRodriguezMoralesFI <eduardo.rodriguez.morales.999@gmail.com>
2025-05-09 09:59:21 -0600

    Construccion De las Solucion para Sistema de Gimnacio

---
+++ SistemaGimnacio/SistemaGimnacio.Test/EntrenadorTest.cs
@@ -0,0 +1,23 @@
+﻿using SistemaGimnasio.Modelos;
+namespace SistemaGimnacio.Test
+{
+    public class EntrenadorTest
+    {
+        [Fact]
+
+        public void AsignarUsuario_DebeAsignarUsuarioALista()
+        {
+            //Arrange
+            var usuario = new Usuario("Juan", 27, "Resistencia");
+            var entrenador = new Entrenador("Carlos","Fuerza" );
+
+            //Act
+            entrenador.AsignarUsuario(usuario);
+
+            //Assert
+
+            Assert.Contains(usuario, entrenador.ObtenerUsuarios());
+        }
+
+    }
+}
---
+++ SistemaGimnacio/SistemaGimnacio.Test/RutinaTests.cs
@@ -0,0 +1,28 @@
+﻿
+using SistemaGimnasio.Modelos;
+namespace SistemaGimnacio.Test
+{
+    public class RutinaTests
+    {
+        [Fact]
+
+        public void AgregarEjercicio_DebeAgregarEjercicioALista()
+        {
+            //Arrange
+
+            var rutina = new Rutina("Fabian Rutina", 20);
+            var ejercicio1 = new Ejercicio("Sentallida hungara", 15,3,2);
+            var ejercicio2 = new Ejercicio("Curl de biceps", 10, 4, 1);
+
+            // Act
+
+            rutina.AgregarEjercicio(ejercicio1);
+            rutina.AgregarEjercicio(ejercicio2);
+
+            // Assert
+
+            Assert.NotEmpty(rutina.ObtenerEjercicios());
+
+        }
+    }
+}
